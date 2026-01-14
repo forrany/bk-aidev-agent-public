97cfda34a57a45bdc6f48ea0618b025d163c0a60
forrany <gaoyaqi411@126.com>
2026-01-14 14:53:51 +0800

    chore: bump version to 1.3.2-beta.6 and update @blueking/ai-ui-sdk dependency to 0.1.19-beta.44

--- src/frontend/ai-blueking/src/types/index.ts
+++ src/frontend/ai-blueking/src/types/index.ts
@@ -34,35 +34,15 @@ import type { useChat } from '@blueking/ai-ui-sdk/hooks';
 import type { Ref, VNode } from 'vue';
 import { AddNewSessionOptions } from '@/store/types';
 
-// 扩展 IAgentCommandComponent 类型，添加 hide 和 selectedText 属性
+// 扩展 IAgentCommandComponent 类型
 // 扩展 mode 属性，用于指定指令模式：simple 简单模式，advanced 高级模式，默认为 advanced模式，在没有明确设置为 simple 模式时，视为 advanced 模式
 export interface IShortcutComponent extends IAgentCommandComponent {
-  selectedText?: string | null;
-
-  // 控制指令组件中单个输入模块的展示模式：simple 简单模式，advanced 高级模式，默认为 advanced模式，在没有明确设置为 simple 模式时，视为 advanced 模式
-  // simple模式：直接展示输入组件，没有label
-  // @since v1.2.9
-  mode?: 'simple' | 'advanced';
-
   /**
-   * 当前输入模块是否显示发送按钮，默认为 false
-   * 如果为 true，则会在输入模块后面追加显示发送按钮，适合简单的输入模块，如：翻译、总结等
+   * 控制指令组件中单个输入模块的展示模式：simple 简单模式，advanced 高级模式，默认为 advanced模式，在没有明确设置为 simple 模式时，视为 advanced 模式
+   * simple模式：直接展示输入组件，没有label
    * @since v1.2.9
    */
-  showSendButton?: boolean;
-
-  /**
-   * 是否将选中的文本填充到该组件中
-   * @since v1.3.2
-   */
-  fillBack?: boolean;
-
-  /**
-   * 用于提取选中文本的正则表达式
-   * 如果设置了该属性，将使用正则表达式从选中文本中提取需要填充的内容
-   * @since v1.3.2
-   */
-  fillRegx?: string;
+  mode?: 'simple' | 'advanced';
 }
 
 // 扩展 IAgentCommand 类型，添加 iconRender 支持
